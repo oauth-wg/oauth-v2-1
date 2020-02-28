cae841f7cf88044c12f651d768ddf4fabca212c5
Aaron Parecki <aaron@parecki.com>
2020-02-28 10:20:19 -0800

    add build script

---
+++ convert-2.1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+kramdown-rfc2629 oauth-2.1.md > build/oauth-2.1.xml
+
+curl https://xml2rfc.tools.ietf.org/cgi-bin/xml2rfc.cgi -F input=@build/oauth-2.1.xml -F 'modeAsFormat=txt/ascii' -F type=ascii > build/oauth-2.1.txt
+curl https://xml2rfc.tools.ietf.org/cgi-bin/xml2rfc.cgi -F input=@build/oauth-2.1.xml -F 'modeAsFormat=html/ascii' -F type=ascii > build/oauth-2.1.html
