adc953d001ba83d6df043ec4b819464f958e9855
Aaron Parecki <aaron@parecki.com>
2020-02-28 10:40:11 -0800

    use local xml2rfc

--- convert-2.1.sh
+++ convert-2.1.sh
@@ -2,5 +2,8 @@
 
 kramdown-rfc2629 oauth-2.1.md > build/oauth-2.1.xml
 
-curl https://xml2rfc.tools.ietf.org/cgi-bin/xml2rfc.cgi -F input=@build/oauth-2.1.xml -F 'modeAsFormat=txt/ascii' -F type=ascii > build/oauth-2.1.txt
-curl https://xml2rfc.tools.ietf.org/cgi-bin/xml2rfc.cgi -F input=@build/oauth-2.1.xml -F 'modeAsFormat=html/ascii' -F type=ascii > build/oauth-2.1.html
+#curl https://xml2rfc.tools.ietf.org/cgi-bin/xml2rfc.cgi -F input=@build/oauth-2.1.xml -F 'modeAsFormat=txt/ascii' -F type=ascii > build/oauth-2.1.txt
+#curl https://xml2rfc.tools.ietf.org/cgi-bin/xml2rfc.cgi -F input=@build/oauth-2.1.xml -F 'modeAsFormat=html/ascii' -F type=ascii > build/oauth-2.1.html
+
+xml2rfc build/oauth-2.1.xml
+xml2rfc --html build/oauth-2.1.xml
