3fa5697b840260388c27f6ac9c19b0ce49ff6d13
Aaron Parecki <aaron@parecki.com>
2020-03-07 09:26:56 -0800

    add generated files

--- md2xml2html.sh
+++ md2xml2html.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-cat draft-parecki-oauth-v2_1.md | kramdown-rfc2629 | xml2rfc -q --v2v3 /dev/stdin -o build/draft-parecki-oauth-v2_1.xml
+kramdown-rfc2629 draft-parecki-oauth-v2_1.md | xml2rfc -q --v2v3 /dev/stdin -o build/draft-parecki-oauth-v2_1.xml
 
 xml2rfc --html --css=build/v3.css build/draft-parecki-oauth-v2_1.xml
+xml2rfc build/draft-parecki-oauth-v2_1.xml
