fee1b00ea2186c1bd45b4047713d321244c075e3
Aaron Parecki <aaron@parecki.com>
2020-03-08 08:46:25 -0700

    consolidate build scripts, remove generated txt and html files

--- md2xml2html.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kramdown-rfc2629 draft-parecki-oauth-v2-1.md | xml2rfc -q --v2v3 /dev/stdin -o build/draft-parecki-oauth-v2-1.xml
-
-xml2rfc --html --css=build/v3.css build/draft-parecki-oauth-v2-1.xml
-xml2rfc build/draft-parecki-oauth-v2-1.xml
