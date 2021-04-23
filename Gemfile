fd1090d79a9f9ab9e01b7f8aa078738d5de97cd2
Aaron Parecki <aaron@parecki.com>
2021-04-23 15:28:46 -0700

    update kramdown libs

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'kramdown-rfc2629'
+gem 'kramdown', ">= 2.3.1"
--- convert-v2-1.sh
+++ convert-v2-1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compile the markdown to v2 XML, then use xml2rfc to convert to v3 XML
-kramdown-rfc2629 draft-ietf-oauth-v2-1.md | xml2rfc -q --v2v3 /dev/stdin -o build/draft-ietf-oauth-v2-1.xml
+bundle exec kramdown-rfc2629 draft-ietf-oauth-v2-1.md | xml2rfc -q --v2v3 /dev/stdin -o build/draft-ietf-oauth-v2-1.xml
 
 # Build the text version
 xml2rfc build/draft-ietf-oauth-v2-1.xml
