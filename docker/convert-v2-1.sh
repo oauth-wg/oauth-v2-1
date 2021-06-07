d22a63d8222edc9814b7c73a9792231150d9bb20
Torsten Lodderstedt <torsten@lodderstedt.net>
2021-06-07 13:51:38 +0200

    added docker-based convert

---
+++ docker/convert-v2-1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Compile the markdown to v2 XML, then use xml2rfc to convert to v3 XML
+kramdown-rfc2629 draft-ietf-oauth-v2-1.md | xml2rfc -q --v2v3 /dev/stdin -o build/draft-ietf-oauth-v2-1.xml
+
+# Build the text version
+xml2rfc build/draft-ietf-oauth-v2-1.xml
+
+# Build the HTML version
+xml2rfc --html --v3 build/draft-ietf-oauth-v2-1.xml
+
+# Submit new draft
+# https://datatracker.ietf.org/submit/
+
